55a910668a77dc7f219406569ecc9fac1ec941c1
Milan <dangi.milan46@gmail.com>
2025-01-25 08:16:45 +0545

    portfolio starting

---
+++ Javascript/main.js
@@ -0,0 +1,36 @@
+// function toggleMenu(){
+//     const menu = document.querySelector(".menu-links");
+//     const icon = document.querySelector(".ham-icon");
+//     menu.classList.toggle("open");
+//     icon.classList.toggle("open");
+// }
+// const projectsContainer= document.getElementById('projectsContainer');
+// const arrowicon = document.querySelector('.right_arrow');
+// function checkOverflow() {
+//     arrowIcon.style.display = projectsContainer.scrollWidth > projectsContainer.clientWidth ? 'block' : 'none';
+// }
+
+// function scrollProjects(direction) {
+//     const scrollAmount = 300; // Adjust the scroll distance as needed
+
+//     if (direction === 'right') {
+//         projectsContainer.scrollLeft += scrollAmount;
+//     } else if (direction === 'left') {
+//         projectsContainer.scrollLeft -= scrollAmount;
+//     }
+
+//     checkOverflow();
+// }
+
+// // Check for overflow on page load and window resize
+// window.addEventListener('load', checkOverflow);
+// window.addEventListener('resize', checkOverflow);
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
